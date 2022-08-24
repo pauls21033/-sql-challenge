1dbe342a27d83ecbf70fb23cb0cc3b032612b57c
pauls21033 <107385310+pauls21033@users.noreply.github.com>
2022-08-23 22:57:03 -0500

    Add files via upload

---
+++ QuickDBD-export.sql
@@ -0,0 +1,62 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL
+);
+
+-- create a table  for the titles
+CREATE TABLE "titles" (
+    "title_id" CHAR(5)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL
+);
+
+-- create a table for the employees
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title" CHAR(5)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "sex" VARCHAR(30)   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
+
+-- create a table for the department employees
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" CHAR(4)   NOT NULL
+);
+
+-- create a table for the department managers
+CREATE TABLE "dept_manager" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+-- create a table for salaries
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" FLOAT   NOT NULL
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
